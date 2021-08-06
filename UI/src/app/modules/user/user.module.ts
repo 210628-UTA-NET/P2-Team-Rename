bc134b754dc812f77be1136b402a7abab59b29f8
William Hardy <william0hardy@gmail.com>
2021-08-06 05:10:17 -0400

    trying out some different templates

--- UI/src/app/modules/user/user.module.ts
+++ UI/src/app/modules/user/user.module.ts
@@ -4,6 +4,8 @@ import { TutorMatchesComponent } from './components/tutor-matches/tutor-matches.
 import { TutorCardComponent } from './components/tutor-card/tutor-card.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TutorSearchV2Component } from './components/tutor-search-v2/tutor-search-v2.component';
+import { TutorSearchV3Component } from './components/tutor-search-v3/tutor-search-v3.component';
 
 
 
@@ -11,7 +13,9 @@ import { CommonModule } from '@angular/common';
   declarations: [
     TutorCardComponent,
     TutorMatchesComponent,
-    TutorSearchComponent
+    TutorSearchComponent,
+    TutorSearchV2Component,
+    TutorSearchV3Component
   ],
   imports: [
     CommonModule,
--- UI/src/app/modules/user/user.routing.ts
+++ UI/src/app/modules/user/user.routing.ts
@@ -1,3 +1,5 @@
+import { TutorSearchV3Component } from './components/tutor-search-v3/tutor-search-v3.component';
+import { TutorSearchV2Component } from './components/tutor-search-v2/tutor-search-v2.component';
 import { TutorCardComponent } from './components/tutor-card/tutor-card.component';
 import { TutorSearchComponent } from './components/tutor-search/tutor-search.component';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,6 +7,8 @@ import { TutorMatchesComponent } from './components/tutor-matches/tutor-matches.
 
 const routes: Routes = [
   { path: 'search', component: TutorSearchComponent },
+  { path: 'searchv2', component: TutorSearchV2Component },
+  { path: 'searchv3', component: TutorSearchV3Component },
   { path: 'matches', component: TutorMatchesComponent },
   { path: 'tutor', component: TutorCardComponent }
 ];
