b77e6b5cb15c2ceebf49a99f487f4cae16d64d5e
syoung908 <syoung908@gmail.com>
2021-08-12 09:46:00 -0700

    db fixes

--- DL/TutorConnectDBContext.cs
+++ DL/TutorConnectDBContext.cs
@@ -68,16 +68,11 @@ namespace DL {
                 .OnDelete(DeleteBehavior.Restrict);
 
             builder.Entity<Tutor>();
+
             builder.Entity<DegreeCertification>();
             builder.Entity<TutorApplication>()
                 .Property(ta => ta.Timestamp)
                 .HasDefaultValueSql("getdate()");
-
-            /*
-            builder.Entity<ChatMessage>()
-                .HasOne(m => m.Sender)
-                .WithMany(u => u.ChatMessages)
-                .HasForeignKey(u => u.SenderId);*/
         }
     }
 }
--- Entities/Database/Message.cs
+++ Entities/Database/Message.cs
@@ -9,12 +9,8 @@ namespace Entities.Database {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
         public User Sender { get; set; }
-
-        [Required]
         public string SenderId { get; set; }
         public User Receiver { get; set; }
-
-        [Required]
         public string ReceiverId { get; set; }
         public string SenderName { get; set; }
         public virtual string Body { get; set; }
