367e32be841a1ac39952b5e2b2b3ff1cadb63226
JSchla01 <penmaler@gmail.com>
2021-08-12 17:31:34 -0400

    Userlist Service

--- UI/src/app/services/userlist.service.ts
+++ UI/src/app/services/userlist.service.ts
@@ -1,9 +1,21 @@
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserDto } from '../models/api/user-dto.model';
+import { EnvironmentUrlService } from './environment-url.service';
+import { environment } from 'src/environments/environment';
+import { UserSearchResponse } from '../models/user/user-search-response.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserlistService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getAllUsers()
+  {
+    return this._http.get<UserSearchResponse>(`${environment.urlAddress}/user/search`);
+  }
 }
