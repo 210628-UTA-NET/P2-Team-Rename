28b808d2f80ea09a847b505bd0661c3bd4d78fa4
syoung908 <syoung908@gmail.com>
2021-08-13 14:19:22 -0700

    Update environment.prod.ts

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  urlAddress: 'https://tutorconnect.azurewebsites.net'
+  urlAddress: 'https://tutorconnectapi.azurewebsites.net',
+  POSI_STACK_KEY: '6bdaf42d460498a853518e98acee44bd',
+  POSI_STACK_URL: 'http://api.positionstack.com/v1/reverse'
 };
